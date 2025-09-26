80bea7f9172f4ac4302e6d2f4565a5287c7a1739
prominecm <sumtzrj7451@hotmail.com>
2025-09-26 16:05:01 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+CONFIG = {
+    'default_slippage': 0.5,  # 0.5%
+    'min_liquidity_threshold': 1000000,  # $1M
+    'max_impermanent_loss_tolerance': 20,  # 20%
+    'preferred_protocols': ['Uniswap V3', 'Curve', 'Balancer'],
+    'risk_free_rate': 2.0,  # 2% 无风险利率
+}
